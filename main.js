de4fdb27a32d31e15dab0c0a5b4c858662ef7906
ser1921 <sponce1921@gmail.com>
2023-05-18 23:27:06 -0300

    creo objetos x medio de un ciclo y los agrupo en un array

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
-const fecha = prompt("ingrese la fecha");
+const fecha = prompt("Ingrese la fecha");
 console.log("Fecha: " + fecha);
 
-let producto = prompt("ingrese nombre del producto - para finalizar escriba esc");
-let cantidad;
-let precio;
+let producto = prompt("Ingrese nombre del producto - para finalizar escriba esc");
+let productos = []; // array para almacenar los productos
 let sumaPrecioTotal = 0;
 
-do {
-    if (producto != "esc"){
-        cantidad = parseFloat(prompt("ingrese cantidad"));
-        precio = parseFloat(prompt("ingrese precio unitario"));
-        let precioTotalItem = multiplicar(cantidad,precio)
-
-        console.log("Item: " + producto + " Cantidad: " + cantidad + " Precio: " + precio + " Precio Total: $ " + precioTotalItem);
-        sumaPrecioTotal += precioTotalItem;
-    }
-    producto = prompt("ingrese nombre del producto - para finalizar escriba esc");
-} while (producto != "esc");
-
-alert("El valor total del inventario ingresado el día " + fecha + " es de " + " $ " + sumaPrecioTotal)
-
-function multiplicar(cantidad, precio){
-    let  precioItem = cantidad * precio
-    return precioItem
+//función para sacar el valor de cada producto
+function multiplicar(cantidad, precio) {
+    return cantidad * precio;
 }
+
+while (producto != "esc") {
+    if (producto != "esc") {
+        let cantidad = parseInt(prompt("Ingrese cantidad"));
+        let precio = parseInt(prompt("Ingrese precio unitario"));
+        let precioTotalItem = multiplicar(cantidad, precio);
+
+        // crear el objeto producto y agregarlo al array "productos"
+        let productoObjeto = {
+            nombre: producto,
+            cantidad: cantidad,
+            precio: precio,
+            precioTotal: precioTotalItem
+        };
+        productos.push(productoObjeto);
+
+        console.log(`Item: ${productoObjeto.nombre} Cantidad: ${productoObjeto.cantidad} Precio: ${productoObjeto.precio} Precio Total: $ ${productoObjeto.precioTotal}`);
+        sumaPrecioTotal += productoObjeto.precioTotal;
+    }
+    producto = prompt("Ingrese nombre del producto - para finalizar escriba esc");
+}
+
+alert(`El valor total del inventario ingresado el día ${fecha} es de $ ${sumaPrecioTotal}`);
+
+//muestra por consola los nombres de los productos dentro del array
+console.log("Nombres de los productos:");
+for (let i = 0; i < productos.length; i++) {
+  console.log(`Producto ${i + 1}: ${productos[i].nombre}`);
+}
+
+
+
 
